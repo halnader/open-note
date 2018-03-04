d4e40461b7f34da994500e5eac8ceeb8d96084d9
leema <leema9321@gmail.com>
2018-03-03 20:35:54 -0500

    fixed boilerplate

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class Dashboard extends React.Component {
+  componentDidMount() {
+
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
   render() {
     return(
-      <div>Test</div>
+      <div>Dash</div>
     )
   }
 }
