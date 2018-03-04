08e4ec4a30d04d8f434126a3b00a5ca4ee2748ae
leema <leema9321@gmail.com>
2018-03-03 19:37:42 -0500

    Created document model

--- models/document_model.js
+++ models/document_model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const DocumentSchema = new mongoose.Schema({
   author: {
-    type: mongoose.Schema.Types.ObjectId
-  }
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User";
+  },
+  rating: Number,
+  name: String,
+  file: String
 });
