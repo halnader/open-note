5eec6319c6fbb847c9c78bd873dff7d69361950f
leema <leema9321@gmail.com>
2018-03-04 12:48:08 -0500

    python stuff

--- client/src/form.js
+++ client/src/form.js
@@ -6,7 +6,7 @@ class Form extends React.Component {
       <div>
         <div className="container">
           <div className="row" style={{margin: "25% 25%", justifyContent: "center"}}>
-            <a className="btn btn-primary" style={{width: "50em", height: "5em", paddingTop: "1.75em", fontSize: "3em", color: "#fff", textAlign: "center"}}>Upload my files!</a>
+            <a className="btn btn-primary" style={{width: "50em", height: "5em", paddingTop: "1.75em", fontSize: "3em", color: "#fff", textAlign: "center"}} href="/api/runpython">Upload my files!</a>
           </div>
         </div>
       </div>
