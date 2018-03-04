4e4281e02c6f875d034cc7b0526e77b8d70084f3
leema <leema9321@gmail.com>
2018-03-03 20:13:39 -0500

    front-end boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './css/index.css';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(promise));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  , document.getElementById('root'));
